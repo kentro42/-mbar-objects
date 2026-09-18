3cefc865bf127495d4ad21d674ce1f5c719b7624
kentro42 <kenethmon42@gmail.com>
2026-09-18 15:34:57 -0500

    Actualizar data.js

--- js/data.js
+++ js/data.js
@@ -1,14 +1,12 @@
 const CLOUD_NAME = "genszcmv";
 
-// arma la URL pública de una imagen a partir de su "public ID" en Cloudinary
 function imagenUrl(publicId){
   return `https://res.cloudinary.com/${CLOUD_NAME}/image/upload/f_auto,q_auto,w_500/${publicId}`;
 }
-// arma la URL pública de un video
 function videoUrl(publicId){
   return `https://res.cloudinary.com/${CLOUD_NAME}/video/upload/f_auto,q_auto/${publicId}.mp4`;
 }
-// toma un fotograma del segundo 0 del video como miniatura (no necesitas subir una foto aparte)
+
 function posterUrl(publicId){
   return `https://res.cloudinary.com/${CLOUD_NAME}/video/upload/so_0,f_jpg,q_auto,w_500/${publicId}.jpg`;
 }
@@ -29,10 +27,9 @@ const objetos = [
     p1: "Esta máquina llegó al local hace más de una década, traída por una clienta que la heredó de su abuela.",
     p2: "Toca el video para verla en movimiento."
   }
-  // agrega aquí el resto de tus piezas siguiendo el mismo formato...
+  
 ];
 
-// arma las URLs finales de cada pieza a partir de su "id" de Cloudinary
 objetos.forEach(o => {
   o.tag = `Pieza ${o.numero}`;
   o.foto = posterUrl(o.id);
